b267aca041b00db75ac29c52d703a2a58ff25f16
George Andrew Joseph <geogaj10@gmail.com>
2025-08-11 09:09:54 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -43,4 +43,47 @@ base_model.trainable = False
 inputs = tf.keras.Input(shape=(224, 224, 3))
 x = data_augmentation(inputs)
 x = base_model(x, training=False)
-x = tf
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+
+model = tf.keras.Model(inputs, outputs)
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_dataset,
+    epochs=EPOCHS,
+    validation_data=validation_dataset
+)
+
+loss, accuracy = model.evaluate(validation_dataset)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+
+plt.savefig('training_history.png')
